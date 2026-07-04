9de29ef92237888eda6fc25e63f36286a81a257d
Juliaassis238 <juliaassis238@users.noreply.github.com>
2026-07-04 08:29:29 +0000

    Fix: update basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const isGithubPages = process.env.NODE_ENV === "production";
-const basePath = isGithubPages ? "/gusman-luxo" : "";
+const basePath = isGithubPages ? "/dra-luciana-granado" : "";
 
 const nextConfig: NextConfig = {
   output: "export",
